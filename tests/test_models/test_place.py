ca8bfdae2da12c13fa29e9d25d41032b3ba079f8
KennyReyesS <kennyreyes2000@gmail.com>
2021-06-30 01:45:12 +0000

    tests

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 """Place Class unittest"""
 import unittest
+from models.base_model import BaseModel
+from models.place import Place
+
+class TestPlace(unittest.TestCase):
+    """ test for place.py """
