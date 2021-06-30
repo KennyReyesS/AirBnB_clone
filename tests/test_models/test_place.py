d394cd26c58fa56165959411c5320cd2a7e42620
KennyReyesS <kennyreyes2000@gmail.com>
2021-06-30 18:48:31 +0000

    more tests added

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -46,3 +46,45 @@ class TestPlace(unittest.TestCase):
         self.assertTrue(hasattr(place, "number_rooms"))
         self.assertEqual(type(place.number_rooms), int)
         self.assertEqual(place.number_rooms, 0)
+
+    def test_number_bathrooms(self):
+        """Test if Place has attr number_bathrooms and it's an int == 0"""
+        place = Place()
+        self.assertTrue(hasattr(place, "number_bathrooms"))
+        self.assertEqual(type(place.number_bathrooms), int)
+        self.assertEqual(place.number_bathrooms, 0)
+
+    def test_max_guest(self):
+        """Test if Place has attr max_guest and it's an int == 0"""
+        place = Place()
+        self.assertTrue(hasattr(place, "max_guest"))
+        self.assertEqual(type(place.max_guest), int)
+        self.assertEqual(place.max_guest, 0)
+
+    def test_price_by_night(self):
+        """Test if Place has attr price_by_night and it's an int == 0"""
+        place = Place()
+        self.assertTrue(hasattr(place, "price_by_night"))
+        self.assertEqual(type(place.price_by_night), int)
+        self.assertEqual(place.price_by_night, 0)
+
+    def test_latitude(self):
+        """Test if Place has attr latitude and it's an float == 0.0"""
+        place = Place()
+        self.assertTrue(hasattr(place, "latitude"))
+        self.assertEqual(type(place.latitude), float)
+        self.assertEqual(place.latitude, 0.0)
+
+    def test_longitude(self):
+        """Test if Place has attr longitude and it's an float == 0.0"""
+        place = Place()
+        self.assertTrue(hasattr(place, "longitude"))
+        self.assertEqual(type(place.longitude), float)
+        self.assertEqual(place.longitude, 0.0)
+
+    def test_amenity_ids(self):
+        """Test if Place has attr amenity_ids and it's an empty list"""
+        place = Place()
+        self.assertTrue(hasattr(place, "amenity_ids"))
+        self.assertEqual(type(place.amenity_ids), list)
+        self.assertEqual(place.amenity_ids, [])
