86249a418e1691280e496963a53d03eca3e67cce
KennyReyesS <kennyreyes2000@gmail.com>
2021-06-30 01:43:32 +0000

    tests

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,3 +1,9 @@
 #!/usr/bim/python3
 """Amenity Class Tests"""
 import unittest
+from base_model import BaseModel
+from models.amenity import Amenity
+
+
+class TestAmenity(unittest.TestCase):
+    """test for amenity.py"""
